2b35e4161f0dd9305eb2018fbab1ed2a0c53838b
Gabriel Gomes <gabrielsogom@gmail.com>
2021-07-06 10:55:29 -0300

    V1.0.6 Implementing aditional data for Each building measurement and limits

--- src/Models/BuildingConsumptionModel.js
+++ src/Models/BuildingConsumptionModel.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const BuildingConsumptionModel = new mongoose.Schema({
     House_id: String,
+    Last_measure_date: Date,
+    Consumption_limit: Number,
+    Last_measure: Number,
     Wattage: Number
 });
 
